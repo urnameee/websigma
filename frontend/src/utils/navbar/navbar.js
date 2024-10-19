41617097723360748c43454fad3061c39ae49119
Shafa <azshafa95@gmail.com>
2024-10-19 15:45:47 +0700

    detailUkm

--- frontend/src/utils/navbar/navbar.js
+++ frontend/src/utils/navbar/navbar.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // Fungsi untuk memuat navbar
     function loadNavbar() {
         // Sesuaikan path ini dengan lokasi navbar.html Anda
-        fetch('/frontend/src/utils/navbar.html')
+        fetch('/frontend/src/utils/navbar/navbar.html')
             .then(response => response.text())
             .then(data => {
-                document.getElementById('navbar-placeholder').innerHTML = data;
+                document.getElementById('navbar').innerHTML = data;
                 initNavbar(); // Panggil fungsi untuk inisialisasi navbar setelah dimuat
             })
             .catch(error => console.error('Error loading navbar:', error));
